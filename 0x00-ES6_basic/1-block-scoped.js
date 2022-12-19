d3213029214ce76d3c7231f1fd9e09e8e0eb4ad6
Yidnekachew Bantrga <mekuyidnedawit@gmail.com>
2022-12-19 20:22:52 +0300

    task-1

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,11 @@
+export default function taskBlock(trueOrFalse) {
+  if (trueOrFalse) {
+    var task = true;
+    var task2 = false;
+  }
+
+  var task = false;
+  var task2 = true;
+
+  return [task, task2];
+}
