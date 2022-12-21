8920f440ebf4685d01193fe840cb8f3339f4e559
Yidnekachew Bantrga <mekuyidnedawit@gmail.com>
2022-12-21 20:23:22 +0300

    update the code format

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,9 +1,9 @@
-export default function handleResponseFromAPI(promise){
+export default function handleResponseFromAPI(promise) {
     return promise
-      .then(() => ({
-        status: 200,
-        body: "Success",
-      }))
-      .catch(() => Error())
-      .finally(() => console.warn("Got a response from the API"));
+        .then(() => ({
+            status: 200,
+            body: "Success",
+        }))
+        .catch(() => Error())
+        .finally(() => console.warn("Got a response from the API"));
 }
