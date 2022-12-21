c71d5ae51ab7885c43dc81338f07a82f7868a382
Yidnekachew Bantrga <mekuyidnedawit@gmail.com>
2022-12-21 23:12:53 +0300

    fix the bug

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,4 +1,4 @@
-//a functio which returns a rejected promise
+/*a functio which returns a rejected promise */
 export default function uploadPhoto(filename) {
     return Promise.reject(new Error(`${filename} cannot be processed`));
 }
