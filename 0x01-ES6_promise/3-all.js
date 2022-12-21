4d443e6c0edde12ebdfaa59453c242b5c5c0391c
Yidnekachew Bantrga <mekuyidnedawit@gmail.com>
2022-12-21 21:48:55 +0300

    fix the code format

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,13 @@
+/* eslint-disable */
+
 import { uploadPhoto, createUser } from "./utils";
 
 export default function handleProfileSignup() {
-    return Promise.all([uploadPhoto(), createUser()])
-        .then((res) => {
-            const { body } = res[0];
-            const { firstName, lastName } = res[1];
-            console.log(`${body} ${firstName} ${lastName}`);
-        })
-        .catch(() => console.log("Signup system offline"));
+  return Promise.all([uploadPhoto(), createUser()])
+    .then((res) => {
+      const { body } = res[0];
+      const { firstName, lastName } = res[1];
+      console.log(`${body} ${firstName} ${lastName}`);
+    })
+    .catch(() => console.log("Signup system offline"));
 }
