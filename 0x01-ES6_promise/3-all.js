750e96f11eeb1135e8bd9d8dce6d691de910caf2
Yidnekachew Bantrga <mekuyidnedawit@gmail.com>
2022-12-21 21:33:12 +0300

    fix the code format

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,11 @@
 import { uploadPhoto, createUser } from "./utils";
 
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])
-    .then((res) => {
-      const { body } = res[0];
-      const { firstName, lastName } = res[1];
-      console.log(`${body} ${firstName} ${lastName}`);
-    })
-    .catch(() => console.log("Signup system offline"));
+    return Promise.all([uploadPhoto(), createUser()])
+        .then((res) => {
+            const { body } = res[0];
+            const { firstName, lastName } = res[1];
+            console.log(`${body} ${firstName} ${lastName}`);
+        })
+        .catch(() => console.log("Signup system offline"));
 }
