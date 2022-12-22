619fcd4eca6a729aebdee41e7f2fff2f62cc60b0
Yidnekachew Bantrga <mekuyidnedawit@gmail.com>
2022-12-22 23:26:37 +0300

    task-10

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,14 @@
+export default class Car{
+    constructor(brand, motor, color){
+        this._brand = brand,
+        this._motor = motor,
+        this._color = color
+    }
+
+
+    cloneCar(){
+        const ClonedCar = this.constructor[Symbol.species] || this.constructor;
+        return new ClonedCar();
+
+    }
+}
