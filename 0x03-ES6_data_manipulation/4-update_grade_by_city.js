006e9ee79b6a2ac6cddd6af11a42b82bfcd9d113
Yidnekachew Bantrga <mekuyidnedawit@gmail.com>
2023-01-03 20:20:11 +0300

    fix the bug

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -6,10 +6,10 @@ export default function updateStudentGradeByCity(
   return listOfStudent
     .filter((student) => student.location === city)
     .map((student) => {
-      const grade = newGrades
+      const gradeOfStudent = newGrades
         .filter((newstudent) => newstudent.studentId === student.id)
         .map((x) => x.grade)[0];
-      const gradeOfStudent = grade || 'N/A';
-      return { ...student, gradeOfStudent };
+      const grade = gradeOfStudent || "N/A";
+      return { ...student, grade };
     });
 }
