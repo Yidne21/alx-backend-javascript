e232a69b1f38b6890646c7912b90bc25ba163bec
Yidnekachew Bantrga <mekuyidnedawit@gmail.com>
2023-01-04 23:29:01 +0300

    task-4 done

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -13,3 +13,33 @@ export interface printTeacheFunction{
 export default function printTeacher(firstName: string, lastName: string): string {
     return `${firstName[0]}. ${lastName}`
 }
+
+export interface IStudentClassConstructor {
+    new(firstName: string, lastName: string): IStudentClass;
+}
+
+export interface IStudentClass {
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+class StudentClass implements IStudentClass{
+    private _firstName!: string;
+    private _lastName!: string;
+    constructor(firstName: string, lastName: string){
+        this._firstName = firstName;
+        this._lastName = lastName;
+    }
+
+    workOnHomework() {
+        return 'Currently working';
+    }
+
+    displayName() {
+        return this._firstName;
+    }
+}
+
+export function createStudent(creator: IStudentClassConstructor, firstName: string, lastName: string): IStudentClass {
+    return new creator(firstName, lastName);
+}
