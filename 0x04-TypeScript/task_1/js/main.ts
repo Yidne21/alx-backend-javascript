2b84f2fc34cb24fb6798f7f3ab8040d757219cb7
Yidnekachew Bantrga <mekuyidnedawit@gmail.com>
2023-01-04 22:26:40 +0300

    task-1 done

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -6,13 +6,3 @@ interface Teacher{
     location: string,
     [index: string]: any;
 }
-
-const teacher3: Teacher = {
-    firstName: 'John',
-    fullTimeEmployee: false,
-    lastName: 'Doe',
-    location: 'London',
-    contract: false,
-};
-
-console.log(teacher3);
