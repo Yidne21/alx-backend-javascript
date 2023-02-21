a1485a97a84b856e6b026ef3a758351f2474dc57
Yidnekachew Bantrga <mekuyidnedawit@gmail.com>
2023-02-21 17:36:31 +0300

    task-0 -> task-2 are done

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * A function which dispaly a message to the STDOUT
+ * @param {String} message the message to be display
+ * @author Yidnekachew Bantrga <https://github.com/Yidne21>
+ */
+const displayMessage = (message) => {
+  console.log(message);
+};
+
+module.exports = displayMessage;
