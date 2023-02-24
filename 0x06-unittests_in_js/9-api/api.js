4b39a8c0d77d16d2807d17657d6036954afab802
Yidnekachew Bantrga <mekuyidnedawit@gmail.com>
2023-02-24 23:22:35 +0300

    task-9 done

---
+++ 0x06-unittests_in_js/9-api/api.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the payment system");
+});
+
+app.get("/cart/:id([0-9]+)", (req, res) => {
+  const id = req.params.id;
+
+  res.send(`Payment methods for cart ${id}`);
+});
+app.listen(7865, () => {
+  console.log("API available on localhost port 7865");
+});
+
+module.exports = app;
